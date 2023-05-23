17ee9f06905ce62b5cb6073c796cd023c87e3c5c
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-23 17:53:24 -0400

    Aligned social icons and applied animations

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -3,10 +3,10 @@ import React from "react";
 import Link from "next/link";
 //import {useRouter} from "next/navigation"
 import Logo from "./Logo";
-import {GithubIcon, LinkedInIcon, BlogIcon} from "./Icons"
+import { GithubIcon, LinkedInIcon, BlogIcon } from "./Icons";
+import { motion } from "framer-motion";
 
 const CustomLink = ({ href, title, className = "" }) => {
-  
   // const router = useRouter();
   // console.log(router)
   // Use this console log to see what the router hook object provides.
@@ -17,11 +17,13 @@ const CustomLink = ({ href, title, className = "" }) => {
     <Link href={href} className={`${className} relative group`}>
       {title}
 
-      <span className={`
+      <span
+        className={`
       h-[3px] inline-block w-0 bg-dark 
       absolute left-0 -bottom-1
       group-hover:w-full transition-[width] ease duration-300
-      `}>
+      `}
+      >
         &nbsp;
       </span>
     </Link>
@@ -38,16 +40,34 @@ const NavBar = () => {
         <CustomLink href="/contact" title="Contact" className="ml-4" />
       </nav>
 
-      <nav>
-        <Link href="https://github.com/baguiar428" target="_blank">
-          <GithubIcon/>
-        </Link>
-        <Link href="https://www.linkedin.com/in/aguiarbruno/" target="_blank">
-          <LinkedInIcon/>
-        </Link>
-        <Link href="https://baguiar428.github.io/" target="_blank">
-          <BlogIcon/>
-        </Link>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://github.com/baguiar428"
+          target="_blank"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/aguiarbruno/"
+          target="_blank"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="https://baguiar428.github.io/"
+          target="_blank"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <BlogIcon />
+        </motion.a>
       </nav>
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
