530559c3a09f38a5671718bb02e2eb483b79ef5d
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-24 23:30:11 -0400

    Fixed SVG issue.Image wasn't vectorized to transparent

--- src/app/components/HireMe.js
+++ src/app/components/HireMe.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { CircularText } from './Icons'
 
 const HireMe = () => {
   return (
-    <div>HireMe</div>
+    <div className='fixed left-4 bottom-4 
+    flex items-center justify-center
+    overflow-hidden'>
+        <div className='w-48 h-auto flex items-center justify-center relative'>
+            <CircularText className={"fill-dark"}/>
+        </div>
+    </div>
   )
 }
 
