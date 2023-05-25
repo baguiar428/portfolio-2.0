c34066f54b6333fa51cdfaf0455a551bd29fdb81
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-24 20:11:40 -0400

    Added Hero section text with animations and resume plus contact links

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Layout from "./components/Layout"
 import Image from "next/image"
+import Link from "next/link"
 import heroPic from "../../public/images/home/hero-pic.png"
+import AnimatedText from "./components/AnimatedText"
 
 export default function Home() {
   return (
@@ -10,12 +12,17 @@ export default function Home() {
           <div className="w-1/2">
             <Image src={heroPic} alt="Bruno Aguiar" className="w-full h-auto" />
           </div>
-          <div className="w-1/2">
-            <h1>Turning Vision Into Reality With Code And Design</h1>
-            <p>
+          <div className="w-1/2 flex-col items-center self-center">
+            <AnimatedText text="Turning Vision Into Reality With Code And Design." 
+             className="!text-6xl !text-left"/>
+            <p className="my-4 text-base font-medium">
               I&apos;m a full-stack developer who recently decided to shift from IT Support to Web Development.
               Check out my latest projects and blog articles and follow me on my journey to making dreams come true!
             </p>
+            <div>
+              <Link href="../public/Bruno_Aguiar-Resume.pdf">Resume</Link>
+              <Link href="mailto:b.aguiar428@gmail.com">Contact Me</Link>
+            </div>
           </div>
         </div>
       </Layout>
