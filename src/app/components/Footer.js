29ad61906baa5fa8c85ab4229ecbf39c73706319
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-24 22:02:49 -0400

    Created Footer.Positioned contents and links

---
+++ src/app/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from "next/link";
+import Layout from './Layout'
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark
+    font-medium text-lg'>
+        <Layout className='py-8 flex items-center justify-between'>
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+            {/* Not needed unless I want to add around "Talk About Bruno" */}
+            {/* <div className='flex items-center'> */}
+            <Link href="/" className="underline underline-offset-2">Talk About Bruno</Link>
+            {/* </div> */}
+            <div>
+            <Link href="mailto:b.aguiar428@gmail.com" target="_blank">Say Hi!</Link>
+            <span className='text-primary text-2xl pl-1'>&#9825;</span>
+            </div>
+        </Layout>
+    </footer>
+  )
+}
+
+export default Footer
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,29 @@
-import NavBar from './components/NavBar'
-import './globals.css'
-import { Montserrat } from 'next/font/google'
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import "./globals.css";
+import { Montserrat } from "next/font/google";
 
-const montserrat = Montserrat({ 
-  subsets: ['latin'],
-  variable: '--font-mont' })
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export const metadata = {
-  title: 'Talk About Bruno',
-  description: 'Portfolio Website',
-}
+  title: "Talk About Bruno",
+  description: "Portfolio Website",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${montserrat.variable} font-sans bg-light w-full min-h-screen`}>
+    <html
+      lang="en"
+      className={`${montserrat.variable} font-sans bg-light w-full min-h-screen`}
+    >
       <body>
-      <NavBar/>
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
