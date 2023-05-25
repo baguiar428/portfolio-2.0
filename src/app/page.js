1d0c76b056e0b36c0b8e1eaca568ad1134e77dba
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-24 20:42:24 -0400

    Added resume pdf and completed functionality of resume and contact link added some animations

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-import Layout from "./components/Layout"
-import Image from "next/image"
-import Link from "next/link"
-import heroPic from "../../public/images/home/hero-pic.png"
-import AnimatedText from "./components/AnimatedText"
+import Layout from "./components/Layout";
+import Image from "next/image";
+import Link from "next/link";
+import heroPic from "../../public/images/home/hero-pic.png";
+import AnimatedText from "./components/AnimatedText";
+import { LinkArrow } from "./components/Icons";
 
 export default function Home() {
   return (
@@ -13,19 +14,36 @@ export default function Home() {
             <Image src={heroPic} alt="Bruno Aguiar" className="w-full h-auto" />
           </div>
           <div className="w-1/2 flex-col items-center self-center">
-            <AnimatedText text="Turning Vision Into Reality With Code And Design." 
-             className="!text-6xl !text-left"/>
+            <AnimatedText
+              text="Turning Vision Into Reality With Code And Design."
+              className="!text-6xl !text-left"
+            />
             <p className="my-4 text-base font-medium">
-              I&apos;m a full-stack developer who recently decided to shift from IT Support to Web Development.
-              Check out my latest projects and blog articles and follow me on my journey to making dreams come true!
+              I&apos;m a full-stack developer who recently decided to shift from
+              IT Support to Web Development. Check out my latest projects and
+              blog articles and follow me on my journey to making dreams come
+              true!
             </p>
-            <div>
-              <Link href="../public/Bruno_Aguiar-Resume.pdf">Resume</Link>
-              <Link href="mailto:b.aguiar428@gmail.com">Contact Me</Link>
+            <div className="flex items-center self-start mt-2">
+              <Link
+                href="/Bruno_Aguiar-Resume.pdf"
+                target="_blank"
+                className="flex items-center bg-dark text-light 
+                p-2.5 px-6 rounded-lg text-lg font-semibold 
+                hover:bg-light hover:text-dark
+                border-2 border-solid border-transparent hover:border-dark"
+                download={true}>
+                Resume
+                <LinkArrow className={"w-6 ml-1"}/>
+              </Link>
+              <Link href="mailto:b.aguiar428@gmail.com" target="_blank"
+              className="ml-4 text-lg font-medium capitalize text-dark underline">
+                Contact Me
+              </Link>
             </div>
           </div>
         </div>
       </Layout>
     </main>
-  )
+  );
 }
