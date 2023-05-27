689d337ce9c8f300d406b3bc723d7e61c774e2c7
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-26 23:14:46 -0400

    Added Skills Section. Animated "skills solar system".

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,39 +1,37 @@
 "use client";
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import Head from "next/head";
 import AnimatedText from "../components/AnimatedText";
-import {useMotionValue, useSpring, useInView} from "framer-motion"
+import { useMotionValue, useSpring, useInView } from "framer-motion";
 import Layout from "../components/Layout";
 import Image from "next/image";
-import bioPic from "../../../public/images/about/bio_pic.png"
+import bioPic from "../../../public/images/about/bio_pic.png";
+import Skills from "../components/Skills";
 
-const AnimatedNumbers = ({value}) => {
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
 
-const ref = useRef(null)
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
 
-const motionValue = useMotionValue(0);
-const springValue = useSpring(motionValue, { duration: 3000})
-const isInView = useInView(ref, {once: true});
-
-useEffect(() => {
-    if(isInView){
-        motionValue.set(value);
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
     }
-}, [isInView, value, motionValue])
+  }, [isInView, value, motionValue]);
 
-useEffect(() => {
+  useEffect(() => {
     springValue.on("change", (latest) => {
-        // console.log(latest)
-        if(ref.current && latest.toFixed(0) <= value) {
-            ref.current.textContent = latest.toFixed(0)
-        }
-    })
-}, [springValue, value])
+      // console.log(latest)
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
 
-
-
-    return <span ref={ref}></span>
-}
+  return <span ref={ref}></span>;
+};
 
 export default function Page() {
   return (
@@ -67,40 +65,55 @@ export default function Page() {
               <p className="font-medium">
                 Technology and tinkering are true passions of mine so I&apos;m
                 always looking out for the latests tech toys to play with. Web
-                development is a large canvas to paint on. <em className="font-bold">Let&apos;s build
-                something beautiful and useful!</em>
+                development is a large canvas to paint on.{" "}
+                <em className="font-bold">
+                  Let&apos;s build something beautiful and useful!
+                </em>
               </p>
             </div>
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
-            bg-light p-8">
-                <div className="absolute top-0 -right-4 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark"/>
-                <Image src={bioPic} alt="Bruno" className="w-full h-auto rounded-2xl"/>
+            <div
+              className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
+            bg-light p-8"
+            >
+              <div className="absolute top-0 -right-4 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+              <Image
+                src={bioPic}
+                alt="Bruno"
+                className="w-full h-auto rounded-2xl"
+              />
             </div>
             {/* Extra Space to work with.*/}
             {/* Either use client and projects counter 
                 or
                 Another picture or graphic*/}
             <div className="col-span-2 flex flex-col items-end justify-between">
-                <div className="flex flex-col items-end justify-center">
-                    <span className="inline-block text-7xl font-bold">
-                        <AnimatedNumbers value={3}/>
-                    </span>
-                    <h2  className="text-xl font-medium capitalize text-dark/75">Satisfied Clients</h2>
-                </div>
-                <div className="flex flex-col items-end justify-center">
-                    <span className="inline-block text-7xl font-bold">
-                        <AnimatedNumbers value={6}/>
-                    </span>
-                    <h2 className="text-xl font-medium capitalize text-dark/75">Projects Completed</h2>
-                </div>
-                <div className="flex flex-col items-end justify-center">
-                    <span className="inline-block text-7xl font-bold">
-                        <AnimatedNumbers value={1}/>
-                    </span>
-                    <h2 className="text-xl font-medium capitalize text-dark/75">year of experience</h2>
-                </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={3} />
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Satisfied Clients
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={6} />
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Projects Completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={1} />
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  year of experience
+                </h2>
+              </div>
             </div>
           </div>
+          <Skills />
         </Layout>
       </main>
     </>
