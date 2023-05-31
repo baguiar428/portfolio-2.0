d1ffbc22ffb2c4e067f056e810d9f394be7e2f9f
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-30 22:25:10 -0400

    Started responsive design.Created hamburger menu and fixed useRouter and asPath issue.Needed to use App Router not Pages Router

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,21 +1,43 @@
 "use client";
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
-//import {useRouter} from "next/navigation"
-import Logo from "./Logo";
+import Logo from "./Logo"; 
 import { GithubIcon, LinkedInIcon, BlogIcon, DribbbleIcon, SunIcon, MoonIcon } from "./Icons";
 import { motion } from "framer-motion";
 import useThemeSwitcher from "./hooks/useThemeSwitcher";
+import { useRouter, usePathname } from 'next/navigation'; //Test usePathname
+
 
 const CustomLink = ({ href, title, className = "" }) => {
   // const router = useRouter();
-  // console.log(router)
-  // Use this console log to see what the router hook object provides.
-  // Use asPath to know what we are on.
-  // ${router.asPath === href ? 'w-full' : 'w-0'}
-
+  const pathName = usePathname();
   return (
     <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`
+      h-[3px] inline-block w-0 bg-dark 
+      absolute left-0 -bottom-1
+      group-hover:w-full transition-[width] ease duration-300
+      ${pathName === href ? "w-full" : "w-0"}
+      dark:bg-light`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  }
+
+  return (
+    <button href={href} className={`${className} relative group`} onClick={handleClick}>
       {title}
 
       <span
@@ -27,17 +49,30 @@ const CustomLink = ({ href, title, className = "" }) => {
       >
         &nbsp;
       </span>
-    </Link>
+    </button>
   );
 };
 
 const NavBar = () => {
 
   const [mode, setMode] = useThemeSwitcher();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between
-    dark:text-light">
+    dark:text-light relative">
+
+    <button className="flex-col justify-center items-center hidden lg:flex" onClick={handleClick}>
+      <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+      <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+      <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+    </button>
+
+      <div className="w-full flex justify-between items-center lg:hidden">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -94,6 +129,73 @@ const NavBar = () => {
         }
         </button>
       </nav>
+      </div>
+
+      {/* Mobile Menu */}
+        {
+          isOpen ?
+          <div className="min-w-[70vw] flex flex-col justify-between items-center z-30 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+          bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
+          <nav className="flex items-center flex-col justify-center text-light dark:text-dark">
+            <CustomMobileLink href="/" title="Home" className="mr-4" toggle={handleClick}/>
+            <CustomMobileLink href="/about" title="About" className="mx-4" toggle={handleClick}/>
+            <CustomMobileLink href="/projects" title="Projects" className="mx-4" toggle={handleClick}/>
+            <CustomMobileLink href="/contact" title="Contact" className="ml-4" toggle={handleClick}/>
+          </nav>
+    
+          <nav className="flex items-center justify-center flex-wrap">
+            <motion.a
+              href="https://github.com/baguiar428"
+              target="_blank"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mr-3"
+            >
+              <GithubIcon />
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/aguiarbruno/"
+              target="_blank"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3"
+            >
+              <LinkedInIcon />
+            </motion.a>
+            <motion.a
+              href="https://baguiar428.github.io/"
+              target="_blank"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 ml-3"
+            >
+              <BlogIcon />
+            </motion.a>
+            <motion.a
+              href="https://dribbble.com/TalkAboutBruno"
+              target="_blank"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 ml-3"
+            >
+              <DribbbleIcon />
+            </motion.a>
+    
+            <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-3 flex items-center justify-center rounded-full p-1
+            ${mode ===  "light" ? "bg-dark text-light" : "bg-light text-dark"}`}>
+            {
+              mode === "dark" ?
+              <SunIcon className={"fill-dark"}/>
+              : <MoonIcon className={"fill-dark"}/>
+            }
+            </button>
+          </nav>
+          </div>
+
+          : null
+        }
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
