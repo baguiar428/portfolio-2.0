6a916344bbd1ba2c820714905f1f8dedd58e7ff4
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-05-31 11:20:42 -0400

    Applied some more styling but hit snag on centering text in home under lg:

--- src/app/components/AnimatedText.js
+++ src/app/components/AnimatedText.js
@@ -32,7 +32,7 @@ const singleWord = {
 const AnimatedText = ({text, className=""}) => {
   return (
     <div className='w-full mx-auto py-2 flex items-center justify-center 
-    text-center overflow-hidden'
+    text-center overflow-hidden sm:py-0'
     >
         <motion.h1 className={`${className} w-full inline-block text-dark font-bold capitalize text-8xl dark:text-light`}
         variants={quote}
--- src/app/page.js
+++ src/app/page.js
@@ -10,18 +10,21 @@ export default function Home() {
   return (
     <main className="flex items-center text-dark w-full min-h-screen
     dark:text-light">
-      <Layout className="pt-0">
-        <div className="flex items-center justify-between w-full">
+      <Layout className="pt-0 md:pt-16 sm:pt-8">
+        <div className="flex items-center justify-between w-full lg:flex-col">
           <div className="w-1/2">
-            <Image src={heroPic} alt="Bruno Aguiar" className="w-full h-auto"
+            <Image src={heroPic} alt="Bruno Aguiar" className="w-full h-auto 
+            lg:hidden md:inline-block md:w-full"
             priority
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
-          <div className="w-1/2 flex-col items-center self-center">
+          <div className="w-1/2 flex-col items-center self-center
+          lg:w-full lg:text-center">
             <AnimatedText
               text="Turning Vision Into Reality With Code And Design."
-              className="!text-6xl !text-left"
+              className="!text-6xl !text-left
+              xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl"
             />
             <p className="my-4 text-base font-medium">
               I&apos;m a full-stack developer who recently decided to shift from
@@ -29,7 +32,7 @@ export default function Home() {
               blog articles and follow me on my journey to making dreams come
               true!
             </p>
-            <div className="flex items-center self-start mt-2">
+            <div className='flex items-center self-start mt-2 lg:self-center'>
               <Link
                 href="/Bruno_Aguiar-Resume.pdf"
                 target="_blank"
