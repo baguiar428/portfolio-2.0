10b64f8d2b8daaa7dc1b97544522fb85be15fe2e
Bruno Aguiar <evolutionsyndicate@gmail.com>
2023-06-01 12:00:45 -0400

    aligned education and experience, changed education text color.

--- src/app/components/Education.js
+++ src/app/components/Education.js
@@ -7,13 +7,13 @@ const Details = ({ type, time, place }) => {
     const ref = useRef(null);
   
     return (
-    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]">
+    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]">
       <LiIcon reference={ref}/>
       <motion.div
       initial={{y:50}}
       whileInView={{y:0}}
       transition={{duration:0.5, type:"spring"}}>
-        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+        <h3 className="capitalize font-bold text-2xl text-primary sm:text-xl xs:text-lg">
           {type}
         </h3>
         <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
--- src/app/components/Experience.js
+++ src/app/components/Experience.js
@@ -7,7 +7,7 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
     const ref = useRef(null);
   
     return (
-    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between
+    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between
     md:w-[80%]">
       <LiIcon reference={ref}/>
       <motion.div
